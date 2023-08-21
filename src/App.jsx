f01a451aad22ee3d0b8fd9123fad461458701f64
Daniel <acidistrict@gmail.com>
2023-08-21 20:20:28 +0100

    feat: add assets and sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./utils/router";
+import "./app.scss";
 
 const App = () => {
-  return (
-    <main>
-      <RouterProvider router={router} />
-    </main>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,12 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/header/Header";
+import "./root-layout.scss";
 
 const RootLayout = () => {
+  const location = useLocation();
+  const currentPath = location.pathname.slice(1);
+
   return (
-    <>
+    <div
+      className={`main-content 
+    ${currentPath === "" && "home"}
+    ${currentPath === "about-us" && "about-us"}
+    ${currentPath === "artists" && "artists"}
+    ${currentPath === "get-in-touch" && "get-in-touch"}
+    ${currentPath.length >= 12 && "artist"}
+    `}
+    >
       <Header />
       <Outlet />
-    </>
+    </div>
   );
 };
 

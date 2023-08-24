e06a3f1f230ddb22bcf4832d01985f2f8fe64250
Daniel <acidistrict@gmail.com>
2023-08-24 18:42:54 +0100

    feat: 404 not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,12 @@
+import "./not-found.scss";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="not-found">
+      <div className="error-code">404</div>
+      <h2>Page not found</h2>
+    </div>
+  );
 };
 
 export default NotFound;

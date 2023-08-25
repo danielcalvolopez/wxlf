60a081cede1c6b79dea8596a2689d9a36ace5718
Daniel <acidistrict@gmail.com>
2023-08-25 19:00:26 +0100

    feat: about us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,17 @@
+import "./about-us.scss";
+
 const AboutUs = () => {
-  return <div>About Us</div>;
+  return (
+    <div className="about-us">
+      <img className="about-us-img" src="/photos/about-us.jpg" alt="" />
+      <p className="about-us-text">
+        Minimalist tattoo studio based in <span> Murcia </span>(Spain). Our goal
+        is to transform your tattoo idea into reality using the most advanced
+        techniques. We guarantee the best quality, so you can rest assure that
+        your tattoo will look spectacular.
+      </p>
+    </div>
+  );
 };
 
 export default AboutUs;

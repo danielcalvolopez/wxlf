6f1f7addbb4f6fb126dd8070ac3b4b0cac964acc
Daniel <acidistrict@gmail.com>
2023-08-30 18:46:16 +0100

    fix: background image fixed

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,8 +1,15 @@
-import { Outlet, useLocation, useRoutes } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/layout/header/Header";
 import "./root-layout.scss";
+import { useEffect, useState } from "react";
 
 const RootLayout = () => {
+  const location = useLocation();
+  const [isArtistPage, setIsArtistPage] = useState(undefined);
+
+  useEffect(() => {
+    location.pathname === "artists";
+  }, []);
   return (
     <div className="main-content">
       <div className="main-content-wrapper">

93646c55c2fcc0480b8c9e8c70433f241d4d1b91
Daniel <acidistrict@gmail.com>
2023-08-30 18:52:14 +0100

    fix: remove residual code

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,15 +1,8 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "../components/layout/header/Header";
 import "./root-layout.scss";
-import { useEffect, useState } from "react";
 
 const RootLayout = () => {
-  const location = useLocation();
-  const [isArtistPage, setIsArtistPage] = useState(undefined);
-
-  useEffect(() => {
-    location.pathname === "artists";
-  }, []);
   return (
     <div className="main-content">
       <div className="main-content-wrapper">
